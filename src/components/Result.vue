a54b7ad2791d51839ce63df1943c38a4a334c9a2
Jorn de Bruin <jorn.bruin@student.graafschapcollege.nl>
2023-11-22 13:03:44 +0100

    volgende vraag met afbeelding en onderwerp

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,31 +1,53 @@
+<script setup>
+import { ref, computed, onMounted } from 'vue';
+import { useRoute, RouterLink } from 'vue-router';
+import router from '../router';
+import quizesData from '@/data/quizes.json';
+import imageMap from '../data/imageImports';
+
+
+const route = useRoute();
+const { quiz } = defineProps(['quiz']);
+const currentQuizId = ref(parseInt(route.params.id, 5));
+const nextQuizId = computed(() => currentQuizId.value + 1);
+
+const navigateToNext = () => {
+  router.push(`/quiz/${nextQuizId.value}`).then(() => {
+    currentQuizId.value = nextQuizId.value;
+    window.location.reload();
+  });
+};
+
+const quizes = ref(quizesData);
+const nextQuiz = computed(() => quizes.value.find((q) => q.id === nextQuizId.value));
+
+
+const quizImage = ref(undefined);
+
+const importImagePath = () => {
+  const imagePath = nextQuiz.value.img.substring(18);
+  quizImage.value = imageMap[imagePath];
+};
+
+onMounted(() => {
+  importImagePath();
+});
+
+</script>
+
 <template>
-    <div class="w-full mt-[430px] pt-10  flex justify-between text-2xl">
-        <button type="button" class=" rounded text-white p-3  bg-pubquiz-primary hover:bg-pubquiz-tertiary ">
-            <RouterLink to="/"><i class="fa-solid fa-house pr-5"></i>Home</RouterLink>
-        </button>
-        <button v-if="route.params.id < 5" type="button" class="rounded text-white p-3  bg-pubquiz-primary hover:bg-pubquiz-tertiary">
-            <RouterLink :to="`/quiz/${nextQuizId}`" @click="navigateToNext">Next<i class="fa-solid fa-arrow-right pl-5"></i></RouterLink>
-        </button>
+  <div class="flex flex-wrap mt-10">
+    <div v-if="nextQuiz" class="text-center">
+      <img :src="quizImage" alt="Quiz Picture" class="mb-2">
+      <h2 class="text-xl font-semibold">{{ nextQuiz.name }}</h2>
     </div>
+  </div>
+  <div class="w-full flex justify-between text-2xl">
+    <button type="button" class=" rounded text-white p-3  bg-pubquiz-primary hover:bg-pubquiz-tertiary ">
+      <RouterLink to="/"><i class="fa-solid fa-house pr-5"></i>Home</RouterLink>
+    </button>
+    <button v-if="route.params.id < 5" type="button" class="rounded text-white p-3  bg-pubquiz-primary hover:bg-pubquiz-tertiary">
+      <RouterLink :to="`/quiz/${nextQuizId}`" @click="navigateToNext">Next<i class="fa-solid fa-arrow-right pl-5"></i></RouterLink>
+    </button>
+  </div>
 </template>
-  
-  <script setup>
-  import { ref, computed } from 'vue';
-  import { useRoute, RouterLink } from 'vue-router';
-  import router from '../router';
-  
-  const route = useRoute();
-  const currentQuizId = ref(parseInt(route.params.id, 5));
-  const nextQuizId = computed(() => currentQuizId.value + 1);
-  
-  const navigateToNext = () => {
-  
-    // Update currentQuizId after the navigation
-    router.push(`/quiz/${nextQuizId.value}`).then(() => {
-      currentQuizId.value = nextQuizId.value;
-      // Force a page refresh
-      window.location.reload(); 
-    });
-  };
-  </script>
-  
---
+++ src/data/imageImports.js
@@ -0,0 +1,15 @@
+import filmImage from '@/assets/images/film.jpg';
+import geschiedenisImage from '@/assets/images/geschiedenis.jpg';
+import sportImage from '@/assets/images/Sport.jpg';
+import aadrijkskundeImage from '@/assets/images/aadrijkskunde.jpg';
+import algemeneKennisImage from '@/assets/images/algemene_kennis.jpg';
+
+const imageMap = {
+  'film.jpg': filmImage,
+  'geschiedenis.jpg': geschiedenisImage,
+  'Sport.jpg': sportImage,
+  'aadrijkskunde.jpg': aadrijkskundeImage,
+  'algemene_kennis.jpg': algemeneKennisImage,
+};
+
+export default imageMap;
