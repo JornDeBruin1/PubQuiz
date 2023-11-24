e169cbda2505b1de996bbc5e9e48b68d7c8fc5e2
Jorn de Bruin <jorn.bruin@student.graafschapcollege.nl>
2023-11-24 10:17:37 +0100

    googleAnalyticsService added for better structure

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import './assets/tailwind.css'
 import { initGoogleAnalytics } from './services/googleAnalyticService';
 
+initGoogleAnalytics();
+
+
 const app = createApp(App)
 
 app.use(router)
---
+++ src/services/googleAnalyticsService.js
@@ -0,0 +1,6 @@
+export function initGoogleAnalytics() {
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-GBMCB139YG');
+}
