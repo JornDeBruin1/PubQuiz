d44a1528042bc234a8bd136d5784e7abf411aae7
Jorn de Bruin <jorn.bruin@student.graafschapcollege.nl>
2023-11-24 10:11:39 +0100

    google-analytics added

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
+import { initGoogleAnalytics } from './services/googleAnalyticService';
 
 const app = createApp(App)
 
